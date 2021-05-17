2925a922aec363776681f5b4a94886f0a42cd623
Eric Squires <eric.g.squires@gmail.com>
2021-05-16 20:54:44 -0400

    fix

--- js/plugin.js
+++ js/plugin.js
@@ -1,25 +1,17 @@
 const Plugin = () => {
-  function updateBackground(slide, default_img) {
-    let img = slide.getAttribute("slides-background-image") || default_img;
-    slide.style.backgroundImage = 'url(' + img + ')';
-  }
-
 	return {
 		id: 'slideBackground',
 		init: reveal => {
       let options = reveal.getConfig().slideBackground || {};
 
       reveal.on('slidechanged', event => {
-        // updateBackground(event.currentSlide, options.img);
-        let img = event.currentSlide.getAttribute("slides-background-image") || options.img;
-        document.querySelector('.reveal .slides').style.backgroundImage = 'url(' + img + ')';
+        if (!reveal.isPrintingPDF()) {
+          let img = event.currentSlide.getAttribute("slides-background-image") || options.img;
+          document.querySelector('.reveal .slides').style.backgroundImage = 'url(' + img + ')';
+        }
       });
 
       reveal.on('ready', event => {
-        // updateBackground(event.currentSlide, options.img);
-        let img = event.currentSlide.getAttribute("slides-background-image") || options.img;
-        document.querySelector('.reveal .slides').style.backgroundImage = 'url(' + img + ')';
-
         if (reveal.isPrintingPDF()) {
           // copy slides-background-image to data-background-image to process
           // bacground images normally in the case of pdfs
@@ -31,6 +23,9 @@ const Plugin = () => {
             slides[i].slideBackgroundContentElement.setAttribute('data-loaded', 'false');
           }
           reveal.sync();
+        } else {
+          let img = event.currentSlide.getAttribute("slides-background-image") || options.img;
+          document.querySelector('.reveal .slides').style.backgroundImage = 'url(' + img + ')';
         }
       });
 
